8e9cf5ed254855a589397bf73f6f03e280bca166
zhouxiyue <15810683906@163.com>
2021-11-05 15:24:31 +0800

    add jedis test

--- redisTest/src/main/java/com/redisTest/App.java
+++ redisTest/src/main/java/com/redisTest/App.java
@@ -2,6 +2,8 @@ package com.redisTest;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.Set;
+
 /**
  * Hello world!
  *
@@ -12,5 +14,20 @@ public class App
     {
         Jedis jedis = new Jedis("127.0.0.1",6379);
         System.out.println(jedis.ping());
+        System.out.println(jedis.exists("username"));
+        System.out.println(jedis.set("username","user1"));
+        System.out.println(jedis.set("password","user1"));
+        Set<String> set = jedis.keys("*");
+        System.out.println(set);
+        System.out.println(jedis.del("username"));
+        System.out.println(jedis.exists("password"));
+        System.out.println(jedis.type("password"));
+        System.out.println(jedis.randomKey());
+        System.out.println(jedis.rename("password","pwd"));
+        System.out.println(jedis.get("pwd"));
+        System.out.println(jedis.select(0));
+        System.out.println(jedis.flushDB());
+        System.out.println(jedis.dbSize());
+        System.out.println(jedis.flushAll());
     }
 }
